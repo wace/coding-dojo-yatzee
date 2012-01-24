6fefcc3a727c9245965ba132662fe748ebef70b5
Capponi nicolas <capponi.nicolas@gmail.com>
2012-01-24 11:00:47 +0100

    cleaning

---
+++ src/main/java/dojo/yatzee/Yatzee.java
@@ -0,0 +1,5 @@
+package dojo.yatzee;
+
+public class Yatzee {
+
+}
--- src/test/java/dojo/yatzee/TestEndToEnd.java
+++ src/test/java/dojo/yatzee/TestEndToEnd.java
@@ -1,4 +1,4 @@
-package com.kelkoo.yahtzee;
+package dojo.yatzee;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -7,29 +7,32 @@ import static org.mockito.Mockito.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import dojo.yatzee.Yatzee;
+
 public class TestEndToEnd {
 
-   // finishes when reach three throws
-   @Test
+   // Finishes when category of one is done
+   @Test 
+   // @Ignore
    public void playWithCategoryOfOnesWithOneUserThreeTurns() throws Exception {
       DiceLauncher diceLauncher = mock(DiceLauncher.class);
-      when(diceLauncher.launch()).thenReturn(new Dices(1, 1, 3, 3, 4)).thenReturn(new Dices(1, 6, 6))
-            .thenReturn(new Dices(1, 2));
-      User user = new User();
-      Yatzee yatzee = new Yatzee(diceLauncher, user);
+      when(diceLauncher.launch()).
+            thenReturn(new Dices(1, 1, 3, 3, 4)).
+            thenReturn(new Dices(1, 6, 6)).
+            thenReturn(new Dices(1, 2));
+      Player joe = new Player();
+      Yatzee yatzee = new Yatzee(diceLauncher, joe);
       yatzee.start();
-      user.selectDices(1, 1);
-      user.wantRethrow();
-      user.selectDices(1);
-      user.wantRethrow();
-      user.selectDices(1);
-      assertThat("should be finished", yatzee.finished(), is(true));
+      joe.selectDices(1, 1);
+      joe.wantToRethrow();
+      joe.selectDices(1);
+      joe.wantToRethrow();
+      joe.selectDices(1);
+      assertThat("game should be finished", yatzee.isFinished(), is(true));
       assertThat(yatzee.score(), is(4));
    }
 
    
-
-   // control no more than three throws
    // manage multiple figures categories (1..6)
    // manage other categories (brelan, carre, full, petite suite, grande suite, yatzee, chance)
    // manage figures bonus
@@ -37,5 +40,4 @@ public class TestEndToEnd {
    // implement interface with terminal
    // implement web interface
    // implement network gaming
-
 }
